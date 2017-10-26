49661f7222c749ae140f568c556c9f8656bcd24f
Sage Ross <ragesoss@gmail.com>
2017-10-26 15:01:47 -0700

    Upgrade lodash to version 4

--- app/assets/javascripts/components/timeline/grading.jsx
+++ app/assets/javascripts/components/timeline/grading.jsx
@@ -16,8 +16,7 @@ const Grading = createReactClass({
   },
 
   render() {
-    // TODO: Change _.sum to _.sumBy when lodash is upgraded to v4.
-    const total = _.sum(this.props.gradeables, 'points');
+    const total = _.sumBy(this.props.gradeables, 'points');
     const gradeables = this.props.gradeables.map((gradeable) => {
       const block = BlockStore.getBlock(gradeable.gradeable_item_id);
       return (
